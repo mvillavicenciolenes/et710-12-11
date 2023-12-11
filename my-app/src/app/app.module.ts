21241fd5e01ac728e3f17db531fa48c186e81acb
EC2 Default User <ec2-user@ip-172-31-28-120.ec2.internal>
2023-12-11 23:08:47 +0000

    For Final

---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { ArtComponent } from './art/art.component';
+import { PhotoComponent } from './photo/photo.component';
+import { AboutComponent } from './about/about.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    ArtComponent,
+    PhotoComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
